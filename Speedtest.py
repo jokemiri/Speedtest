6721e243cab3e39e9253c562bf6cf9ad7821f245
jokemiri <jokemiri9@gmail.com>
2023-02-17 22:29:05 +0000

    second commit

--- Speedtest.py
+++ Speedtest.py
@@ -2,11 +2,21 @@ import speedtest
 import tkinter as tk
 import messagebox
 
-def download_speed():
-    print("Button Clicked")
+def button_click(args):
+    if args == 1:
+        down_speed = (st.download()/(1025*1025),"Mbps")
+        dspeed = "Your download speed is {}"
+        print(dspeed.format(down_speed))
+    elif args == 2:
+        messagebox.showinfo(st.upload()/(1025*1025),"Mbps")
+    # if args == 3:
+    # elif args == 3:
+    #     servername=[]
+    #     st.get_servers(servername)
+    #     messagebox.showinfo(st.results.ping)
+        
 
-def upload_speed():
-    print("Button 2 Clicked")
+
 
 st=speedtest.Speedtest()
 
@@ -18,10 +28,13 @@ root.title("Speedtest")
 root.configure(background='#154c79')
 
 download_button = tk.Button(root, text='Download Speed', font='Candara, 14', 
- background='white', foreground='#154c79', command=download_speed).place(x=30, y=30)
+ background='white', foreground='#154c79', command=lambda:button_click(1)).place(x=50, y=30)
 
 upload_button = tk.Button(root, text='Upload Speed', font='Candara, 14', 
- background='white', foreground='#154c79', command=upload_speed).place(x=250, y=30)
+ background='white', foreground='#154c79', command=lambda:button_click(2)).place(x=250, y=30)
+
+# ping_button = tk.Button(root, text='Ping', font='Candara, 14', 
+#  background='white', foreground='#154c79', command=lambda:button_click(3)).place(x=300, y=30)
 
 # option=int(input('''What speed do you want to measure:
 # 1) Download Speed
